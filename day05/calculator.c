b07871744e86d3add66ce56727bd44accc75dbb4
Aditya Vinayak Sngh <Aditya.40968@stu.upes.ac.in>
2026-09-20 02:05:19 +0530

    Implement basic switch case calculator in C

---
+++ day05/calculator.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2, result;
+
+    printf("--- Day 5: Switch Case Calculator ---\n");
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            printf("Result: %.2lf + %.2lf = %.2lf\n", num1, num2, result);
+            break;
+
+        case '-':
+            result = num1 - num2;
+            printf("Result: %.2lf - %.2lf = %.2lf\n", num1, num2, result);
+            break;
+
+        case '*':
+            result = num1 * num2;
+            printf("Result: %.2lf * %.2lf = %.2lf\n", num1, num2, result);
+            break;
+
+        case '/':
+            if (num2 != 0.0) {
+                result = num1 / num2;
+                printf("Result: %.2lf / %.2lf = %.2lf\n", num1, num2, result);
+            } else {
+                printf("Error: Division by zero is not allowed.\n");
+            }
+            break;
+
+        default:
+            printf("Error: Invalid operator '%c'. Please use +, -, *, or /.\n", operator);
+            break;
+    }
+
+    return 0;
+}
