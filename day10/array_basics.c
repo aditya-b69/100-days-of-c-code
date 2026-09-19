a0e6efaca25817950c30c1cd4c2e79aacd269a15
Aditya Vinayak Sngh <Aditya.40968@stu.upes.ac.in>
2026-09-20 02:17:45 +0530

    Implement array basics with min, max, and average

---
+++ day10/array_basics.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d integers:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int min = arr[0], max = arr[0];
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] < min) min = arr[i];
+        if (arr[i] > max) max = arr[i];
+        sum += arr[i];
+    }
+
+    printf("Min: %d | Max: %d | Average: %.2f\n", min, max, (float)sum / n);
+    return 0;
+}
