b2be16ebce482c872b5b469b3be8c824331b2ef1
Aditya Vinayak Sngh <Aditya.40968@stu.upes.ac.in>
2026-09-24 17:31:21 +0530

    Create char_count.c

---
+++ day30/char_count.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char str[150];
+    int vowels = 0, consonants = 0, digits = 0, spaces = 0;
+
+    printf("--- Day 30: Character Analysis ---\n");
+    printf("Enter a line of text: ");
+    scanf(" %149[^\n]", str);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        char ch = tolower(str[i]);
+
+        if (ch >= 'a' && ch <= 'z') {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                vowels++;
+            else
+                consonants++;
+        } else if (ch >= '0' && ch <= '9') {
+            digits++;
+        } else if (ch == ' ') {
+            spaces++;
+        }
+    }
+
+    printf("Vowels: %d\nConsonants: %d\nDigits: %d\nSpaces: %d\n",
+           vowels, consonants, digits, spaces);
+    return 0;
+}
